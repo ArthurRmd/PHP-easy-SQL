282f6e4c80b49e5f0a4176d93e60ac85cc283bd8
Arthur <arthur.remond@protonmail.com>
2019-04-27 19:13:11 +0200

    add class DbConfig

---
+++ DbConfig.php
@@ -0,0 +1,50 @@
+<?php
+
+
+class DbConfig
+{
+
+    protected $data;
+
+    function __construct()
+    {
+        $fichier = file_get_contents("config.json");
+        $fichier = json_decode($fichier);
+
+        $this->data['sgbd'] = $fichier->sgbd;
+        $this->data['serveur'] = $fichier->serveur;
+        $this->data['login'] = $fichier->login;
+        $this->data['pass'] = $fichier->pass;
+        $this->data['table'] = $fichier->table;
+    }
+
+    public function getData($data = null)
+    {
+        if ($data == null) {
+            return $this->data;
+        } elseif (isset($this->data[$data])) {
+            return $this->data[$data];
+        } else {
+            return null;
+        }
+    }
+
+    public function getDns()
+    {
+        return $this->getData('sgbd') . ":host=" . $this->getData('serveur') . ";dbname=" . $this->getData('table');
+    }
+
+    public function getPdo()
+    {
+
+        try {
+            $this->pdo = new PDO($this->getDns(), $this->getData('login'), $this->getData('pass'));
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->pdo;
+        } catch (PDOException $e) {
+            // echo "Erreur connexion à la base de donnée -> " . $e->getMessage();
+            return null;
+        }
+    }
+
+}
